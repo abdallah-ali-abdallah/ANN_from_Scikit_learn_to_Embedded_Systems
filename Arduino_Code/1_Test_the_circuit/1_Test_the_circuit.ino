92de7eaea196a819f790717e380a453b8a87b757
Abdallah Ali <abdallah.ali.abdallah.elmasry@gmail.com>
2019-11-02 16:52:55 +0200

    modify the first example

--- Arduino_Code/1_Test_the_circuit/1_Test_the_circuit.ino
+++ Arduino_Code/1_Test_the_circuit/1_Test_the_circuit.ino
@@ -1,3 +1,4 @@
+// Just a simple test for the HW
 #include <Arduino.h>
 
 #define     greenLed     5
@@ -5,11 +6,6 @@
 #define     blueLed      7
 #define     LDR_PIN      A0
 
-// Array to hold the color response for the LDR
-volatile uint16_t color_resp[3] = {0,0,0};
-
-void printLDR_response();
-void capture_color_response();
 
 void setup()
 {
@@ -23,37 +19,18 @@ void setup()
 
 void loop()
 {
-    capture_color_response();
-    printLDR_response();
-    Serial.println("label");
-
-}
-
-void capture_color_response(){
-    
-    digitalWrite(redLed,HIGH);
-    delay(50);
-    color_resp[0] = analogRead(LDR_PIN);
-    digitalWrite(redLed,LOW);
+    Serial.println(analogRead(LDR_PIN));
     
     digitalWrite(greenLed,HIGH);
-    delay(50);
-    color_resp[1] = analogRead(LDR_PIN);
+    delay(500);
     digitalWrite(greenLed,LOW);
-
+    delay(500);
+    digitalWrite(redLed,HIGH);
+    delay(500);
+    digitalWrite(redLed,LOW);
+    delay(500);
     digitalWrite(blueLed,HIGH);
-    delay(50);
-    color_resp[2] = analogRead(LDR_PIN);
+    delay(500);
     digitalWrite(blueLed,LOW);
-
-}
-
-void printLDR_response(){
-    Serial.print(color_resp[0]);
-    Serial.print(",");
-    Serial.print(color_resp[1]);
-    Serial.print(",");
-    Serial.print(color_resp[2]);
-    Serial.print(",");
-
+    delay(500);
 }
